66ed78ae1654f0aecc2243e5dbe9f4832eb9527f
Filatov Andrey <swfas@ya.ru>
2016-10-13 23:25:55 +0300

    Added the uploaded files

---
+++ httpdocs/uploads/documents/5ca6711e8af7914ed2e0fc090ef6b05fae4c158a/register[1].php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+<title>Event System</title>
+<link href="/include/styles.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+	<div class="content">
+		<h1>
+			Event System
+		</h1>
+<div class="information">
+	Welcome to the Event System.
+	<p>
+	Using this online system you can submit an abstract or register and book hotel accommodation for an event.  Please use the drop-down menus below as directed to access each event.
+	</p>
+	You can view the different events currently available by selecting an event below.
+	
+	<br />
+</div>
+<form action="index.php" method="POST">
+
+		<div class="selectEvent">
+			<h3>
+				&nbsp;
+			</h3>
+			<label class="two" for="select_register_event">
+			Select an event to register &amp; book hotel accommodation
+			</label>
+			<select name="select_register_event" id="select_register_event" >
+				<option value="36">18th EFAS Basic Instructional Course,13-14 October 2016, Scandic Triangeln Hotel, Malmo, Sweden</option><option value="35">6th EFAS Advanced Cadaver Course, 8th-9th September 2016, Barcelona</option><option value="37">7. D.A.F. DRG Seminar, 22 Juni 2016, Urania Berlin</option><option value="31">D.A.F. REGISTRATION FOOT INTERNATIONAL 2016</option><option value="32">EFAS REGISTRATION FOOT INTERNATIONAL 2016</option><option value="33">i-FAB REGISTRATION FOOT INTERNATIONAL 2016</option>			</select>
+			<input type="submit" name="register_event" id="register_event" value="Go"/><br />
+		</div>
+</form>
+		<br />
+		<br />
+		<br />
+	</div>
+	<div class="footer">
+		The Event System
+	</div>
+</body>
+</html>
